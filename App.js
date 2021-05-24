3557557604f5a9fae919873235c1cac761134003
Isaac Lee <hlee777@wisc.edu>
2021-05-24 18:33:12 +0800

    new Meetcha

--- App.js
+++ App.js
@@ -1,13 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
+
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
+  function ChatScreen() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Chat!</Text>
+      </View>
+    );
+  }
+  function HomeScreen() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Home!</Text>
+      </View>
+    );
+  }
+  
+  function ProfileScreen() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Profile!</Text>
+      </View>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Tab.Navigator tabBarOptions={{ showLabel: false }}>
+    <Tab.Screen name="Chat" 
+                component={ChatScreen}
+                options={{
+                  tabBarIcon: ()=> (<FontAwesome name={'comment'} size={30} color={'#FEC357'} />),
+                }} />
+      <Tab.Screen name="Home" 
+                  component={HomeScreen}
+                  options={{
+                    tabBarIcon: ()=> (<FontAwesome name={'home'} size={40} color={'#FEC357'} />),
+                  }} />
+      <Tab.Screen name="Profile" 
+                  component={ProfileScreen}
+                  options={{
+                    tabBarIcon: ()=> (<FontAwesome name={'user'} size={30} color={'#FEC357'} />),
+                  }} />
+    </Tab.Navigator>
+  </NavigationContainer>
   );
 }
 
